b72ff363fcf81caf45c01689c52126af0c3d4c0f
Vivian Velazquez <vivianmvero@gmail.com>
2025-10-08 20:05:51 -0600

    Eliminar

---
+++ Practica1/PracticaProgramada1DAL/Entidades/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaProgramada1DAL.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+    public class Telefono
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public string Tipo { get; set; } // Como "Casa", "Trabajo", "Móvil"
+    }
+
+        // Lista para hacer la relación de un cliente tiene varios teléfonos
+        public List<Telefono> Telefonos { get; set; } = new List<Telefono>();
+    }
+}
